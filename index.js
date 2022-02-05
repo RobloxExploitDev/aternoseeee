04b3c5324a3183cde12632d27a31e385c1846dc7
RobloxExploitDev <88513019+RobloxExploitDev@users.noreply.github.com>
2022-02-05 20:26:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ bot.loadPlugin(cmd)
 
 bot.on('login',function(){
 	console.log("Logged In")
-	bot.chat("Hello! DJxDevs AFK bot Here!");
+	bot.chat("hello");
 });
 
 bot.on('time', function(time) {
